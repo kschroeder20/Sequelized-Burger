37e065e9c11fb5ce117acd75571d0133591cd783
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-01 10:29:45 -0400

    finished converting project to sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,38 @@
-// This file uses the orm to make the changes to the db
-var orm = require("../config/orm");
+// // This file uses the orm to make the changes to the db
+// var orm = require("../config/orm");
 
-var burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            // Pass through to the controller
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            // Pass through to the controller
-            cb(res);
-        });
-    },
-    update: function (objColVals, condtion, cb) {
-        orm.update("burgers", objColVals, condtion, function (res) {
-            // Pass through to the controller
-            cb(res);
-        });
-    }
+// var burger = {
+//     all: function (cb) {
+//         orm.all("burgers", function (res) {
+//             // Pass through to the controller
+//             cb(res);
+//         });
+//     },
+//     // The variables cols and vals are arrays.
+//     create: function (cols, vals, cb) {
+//         orm.create("burgers", cols, vals, function (res) {
+//             // Pass through to the controller
+//             cb(res);
+//         });
+//     },
+//     update: function (objColVals, condtion, cb) {
+//         orm.update("burgers", objColVals, condtion, function (res) {
+//             // Pass through to the controller
+//             cb(res);
+//         });
+//     }
+// };
+
+// // Export the database functions for the controller
+// module.exports = burger;
+
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: DataTypes.STRING,
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    return Burger;
 };
-
-// Export the database functions for the controller
-module.exports = burger;
--- server.js
+++ server.js
@@ -1,39 +1,85 @@
-// Dependencies
+// // Dependencies
+// var express = require("express");
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
+// var db = require("./models");
+
+// // Create an instance of the express app.
+// var app = express();
+
+// // Set the port of our application
+// // process.env.PORT lets the port be set by Heroku
+// var PORT = process.env.PORT || 8080;
+
+// //Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static(__dirname + '/public'));
+
+// //Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+
+// //Parse application/json
+// app.use(bodyParser.json());
+
+// // Set Handlebars as the default templating engine.
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller");
+
+// app.use(routes);
+
+// db.sequelize.sync({ force: true }).then(function () {
+//     app.listen(PORT, function () {
+//         // Log (server-side) when our server has started
+//         console.log("Server listening on: http://localhost:" + PORT);
+//     });
+// });
+
 var express = require("express");
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var bodyParser = require("body-parser");
 
-// Create an instance of the express app.
+// Sets up the Express App
 var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
+// EXPRESS
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// BODY-PARSER
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + "/public"));
 //Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 //Parse application/json
 app.use(bodyParser.json());
 
+// HANDLEBARS
 // Set Handlebars as the default templating engine.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
+// ROUTES
+require("./controllers/burgers_controller")(app);
 
-app.use(routes);
-
-
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+// Syncing our sequelize models and then starting our Express app
+db.sequelize
+  .sync({
+    force: true
+  })
+  .then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
