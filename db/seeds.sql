5bf679b2b129535ce64ffd8ae1b465bc2a048f12
Kevin Schroeder <schroederkevin94@gmail.com>
2019-06-01 08:14:47 -0400

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- This file creates initial entries to database
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Big Mac', true),
+    ('Slider', false),
+    ('Wopper', true);
---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// This file uses the orm to make the changes to the db
+var orm = require("../config/orm");
+
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            // Pass through to the controller
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            // Pass through to the controller
+            cb(res);
+        });
+    },
+    update: function (objColVals, condtion, cb) {
+        orm.update("burgers", objColVals, condtion, function (res) {
+            // Pass through to the controller
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller
+module.exports = burger;
